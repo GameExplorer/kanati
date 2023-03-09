da5ab9dfffc6a50c7b8b10862cc62a7d116f76ae
GameExplorer <73129125+GameExplorer@users.noreply.github.com>
2023-03-09 22:19:14 +0100

    One Pager v.0.3
    - removed some obscure code
    - added logo and top page (to be done)
    - added vector.png (signature)
    - all text is by default now white, it is not anymore black
    - spagethi code at top needs to be cleaned

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,43 @@
   import "../app.css";
 </script>
 <!-- vrh strani + logo -->
-<div class="px-16">
-  <div class="flex flex-wrap">
-    <img class="object-contain w-40 h-36"
-      src="src/images/logo.png" alt="logo" />
+<div class="ml-12 mt-6 w-1/6">
+<div class="flex flex-wrap items-center">
+  <img class="object-cover w-32 sm:w-40 h-auto mr-4 mt-2" src="src/images/logo.png" alt="logo" />
+</div>
+</div>
+<div class="grid grid-cols-2 gap-4 pt-2">
+  <div class="border-r-2 pr-24 border-gray-700 pr-12">
+    <div class="px-16 pt-4">
+      <div class="text-white text-2xl md:text-5xl lg:text-5xl px-40 pt-16">
+        <span class="px-12">Svetujemo,</span><br>
+        <span class="px-4">investiramo in</span><br> 
+        <span class="-ml-14">ustvarjamo </span><span class="font-georgiaI italic text-[#C1DDBB]">možnosti<br></span>
+        <span class="px-4 font-georgiaI italic text-[#C1DDBB]">za realizacijo</span>
+      </div>
+      <div>
+        <img class="py-8 ml-24 w-8/12 pb-8" src="src/images/Vector.png" alt="signature"/>
+      </div>
+      <!--random sponsors -->
+      <div></div>
+      <div class="mt-32"></div>
+    </div>
+  </div>
+  <div>
+    <div class="pt-16 lg:pt-36 text-center">
+      <div class="lg:text-5xl">$ 1.0 M</div>
+      <div class="text-gray-300 pb-16">Investiranih do danes</div>
+      <div class="lg:text-5xl">8 - 12%</div>
+      <div class="text-gray-300">letni optimalni<br> doprinos</div>
+    </div>    
   </div>
 </div>
 
+
+
 <!-- trg-->
-<div class="bg-black py-10 px-16 md:flex md:flex-col md:items-start">
-  <div class="text-white text-left mb-6">
+<div class="py-10 px-16 md:flex md:flex-col md:items-start">
+  <div class="text-white text-left mb-6 pt-32">
     <p class="font-georgia text-2xl md:text-3xl lg:text-4xl">Trg kaže pot sprememb,<br>
       odločitev in priložnosti. Odnosi<br>
       in sodelovanja so bistvo rasti<br>
@@ -20,7 +47,7 @@
 </div>
 
 <!-- Investiranje -->
-<div class="bg-black py-10 px-8 md:flex md:flex-col" style="text-align:right; margin-right:15%;">
+<div class="py-10 px-8 md:flex md:flex-col" style="text-align:right; margin-right:15%;">
   <div class="text-white mb-4">
     <p class="font-georgia text-xl md:text-3xl lg:text-xl">Investiranje v nepremičnine, projekte,<br>
       potencialna podjetja in njihove izdelke so<br>
@@ -29,7 +56,7 @@
 </div>
 
 <!-- delaj pametno-->
-<div class="bg-black py-12 px-16 md:flex md:flex-col md:items-start">
+<div class="py-12 px-16 md:flex md:flex-col md:items-start">
   <div class="text-white text-left mb-4">
     <p class="font-georgia text-5xl md:text-3xl lg:text-5xl">Delaj pametno, zavestno in</p>
   </div>
@@ -41,12 +68,12 @@
 
 
 <!-- sodelovanje -->
-<div class="bg-black py-24 px-16 md:flex md:flex-col">
+<div class="py-24 px-16 md:flex md:flex-col">
   <div class="flex flex-wrap ">
     <img
       src="src/images/apostrof.png" alt="apostrofa" />
   </div>
-  <div class="text-white text-left mt-8 mb-4">
+  <div class="text-left mt-8 mb-4">
     <p class="font-georgia  text-4xl md:text-3xl lg:text-5xl">Sodelovanje z ljudmi, ki so<br>
       v svojem področju najbolj uspešni,<br>
       je naša največja prednost</p>
@@ -55,7 +82,7 @@
 
 <!-- vloge + slika-->
 <div class="flex items-right py-12">
-  <div class="text-white px-16 text-2xl md:text-xl lg:text-3xl">
+  <div class="px-16 text-2xl md:text-xl lg:text-3xl">
     <div>Z naprednim sistemom</div>
     <div>dela povezujemo,</div>
     <div>združujemo in rastemo</div>
@@ -73,11 +100,11 @@
 <!-- box s številkam -->
 
 <!-- portfolio (trenutko blank)-->
-<div class="bg-black py-40"></div>
+<div class="py-40"></div>
 
 <!-- realizacija -->
-<div class="bg-black py-10 px-4 md:flex md:flex-col">
-  <div class="text-white text-center mb-4">
+<div class="py-10 px-4 md:flex md:flex-col">
+  <div class="text-center mb-4">
     <p class="font-georgia text-4xl md:text-3xl lg:text-8xl">Uspešna realizacija je</p>
   </div>
   <div class="text-[#D2D0FB] text-center">
@@ -86,11 +113,11 @@
 </div>
 
 <!-- footer -->
-<div class="bg-black py-10 px-4 md:flex md:flex-col text-center">
-  <div class="text-white text-center mb-4">
+<div class="py-10 px-4 md:flex md:flex-col text-center">
+  <div class="text-center mb-4">
     <p class="font-georgia regular text-xl md:text-lg lg:text-base">info@kanati.si</p>
   </div>
-  <div class="text-white text-center">
+  <div class="text-center">
     <p class="font-georgia text-xl md:text-lg lg:text-base">+386 40 123 456</p>
   </div>
 </div>
